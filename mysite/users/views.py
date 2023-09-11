9fdb1be5ab5304b4a0ddcabd156388ea096cc14c
Clark Donnell <thomas.donnell01@gmail.com>
2023-09-11 12:24:21 -0500

    add login no auth

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -19,3 +19,18 @@ def register(request):
             account.save()
     context = {'form':form, 'account_form':account_form}
     return render(request, "users/registration.html", context)
+
+def login(request):
+    form = CreateUserForm()
+    account_form = AccountForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        account_form = AccountForm(request.POST)
+        if form.is_valid() and account_form.is_valid():
+            user = form.save()
+            account = account_form.save(commit=False)
+            account.user = user
+            account.save()
+    context = {'form':form, 'account_form':account_form}
+    return render(request, "users/login.html", context)
