4375d25d238a97aae0b36d0869bcc077072ac002
Clark Donnell <thomas.donnell01@gmail.com>
2023-09-24 03:12:14 -0500

    added a base html

--- mysite/students/static/students/js/discussion.js
+++ mysite/students/static/students/js/discussion.js
@@ -2,12 +2,12 @@
 
 const btnDiv = document.getElementById('addbtn');
 const courseDiv = document.getElementById('coursediv');
-const postWrapper = document.getElementById('posts-wrapper');
+const content = document.getElementById('content');
 // Add a click event listener to the trigger div
 btnDiv.addEventListener('click', function () {
     // Toggle the display style of the target div
     courseDiv.style.display = 'flex';
-    postWrapper.style.display = 'none';
+    content.style.display = 'none';
 });
 
 const divs = document.querySelectorAll('.posts');
